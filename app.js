00fd0bee8e4666f8f95f7b505c817b9fa69d6492
Muhammad Ahmed <ahmed.awan003@gmail.com>
2025-04-14 08:52:52 +0500

    Client Web

--- app.js
+++ app.js
@@ -30,3 +30,52 @@ var swiper = new Swiper(".mySwiper", {
       clickable: true,
     },
   });
+
+
+//   $('.owl-carousel').owlCarousel({
+//     loop:true,
+//     margin:10,
+//     nav: true,           // 👈 This enables next/prev buttons
+//     dots: true, 
+//     responsive: {
+//       0: {
+//           items: 1,
+//       },
+//       430: {
+//           items: 2, 
+//       },
+//       768: {
+//           items: 3, 
+//       },
+//       992: {
+//           items: 4,
+//       }
+//     }
+// })
+
+$('.owl-carousel').owlCarousel({
+  loop:true,
+  margin: 30,
+  items: 4,
+  nav:true,
+  autoplay: true,
+  smartSpeed: 1500,
+  dots:true,
+  navContainerClass: 'cake_feature_slider',
+  navText: ['<i class="fa fa-angle-left" aria-hidden="true"></i>','<i class="fa fa-angle-right" aria-hidden="true"></i>'],
+  responsiveClass: true,
+  responsive: {
+      0: {
+          items: 1,
+      },
+      430: {
+          items: 2, 
+      },
+      768: {
+          items: 3, 
+      },
+      992: {
+          items: 4,
+      }
+  }
+})
