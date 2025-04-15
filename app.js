b099304c26f16178c9902139587d492f0fe48ac6
Muhammad Ahmed <ahmed.awan003@gmail.com>
2025-04-15 09:50:45 +0500

    Client Web

--- app.js
+++ app.js
@@ -1,3 +1,14 @@
+
+
+
+AOS.init({
+    duration: 1000, // animation ki speed
+    offset: 120,
+    once: true    // kitni scroll ke baad start ho    // sirf 1 dafa animate kare
+  });
+
+
+
 window.addEventListener("scroll", function () {
     var navbar = document.querySelector(".top-header-2");
 
@@ -40,7 +51,7 @@ $('.owl-carousel').owlCarousel({
   nav:true,
   autoplay: true,
   smartSpeed: 1500,
-  dots:true,
+  dots:false,
   navContainerClass: 'cake_feature_slider',
   navText: ['<i class="fa fa-angle-left" aria-hidden="true"></i>','<i class="fa fa-angle-right" aria-hidden="true"></i>'],
   responsiveClass: true,
@@ -59,3 +70,82 @@ $('.owl-carousel').owlCarousel({
       }
   }
 })
+
+var defaultSelector = $(".cake-filter ul li.active").attr("data-filter");
+if (defaultSelector) {
+    $(".grid_portfolio_area").isotope({
+        filter: defaultSelector,
+        animationOptions: {
+            duration: 450,
+            easing: "linear",
+            queue: false,
+        }
+    });
+}
+
+
+portfolio_isotope();
+
+
+
+
+function portfolio_isotope(){
+  if ( $('.cake-filter ul li').length ){
+      // Add isotope click function
+      $(".cake-filter ul li").on('click',function(){
+          $(".cake-filter ul li").removeClass("active");
+          $(this).addClass("active");
+
+          var selector = $(this).attr("data-filter");
+          $(".grid_portfolio_area").isotope({
+              filter: selector,
+              animationOptions: {
+                  duration: 450,
+                  easing: "linear",
+                  queue: false,
+              }
+          });
+          return false;
+      });
+  }
+}
+
+portfolio_isotope();
+
+
+
+// var count = 1;
+// var max = 11;
+
+// function startCounter() {
+//   var countElement = document.getElementById('count');
+//   var interval = setInterval(function() {
+//     countElement.innerText = count;
+//     count++;
+//     if (count > max) {
+//       clearInterval(interval);
+//     }
+//   }, 300); // Delay ko slow ya fast karne ke liye number change karo
+// }
+
+// startCounter()
+
+
+
+function animateCount(id, number, duration = 4000) {
+  $(id).animateNumber({ number: number }, duration);
+}
+
+var counterStarted = false;
+
+$('#counter').waypoint(function(direction) {
+  if (direction === 'down' && !counterStarted) {
+    animateCount('#counter', 11);
+    animateCount('#counter1', 100);
+    animateCount('#counter2', 30);
+    animateCount('#counter3', 1500);
+    counterStarted = true; // only once
+  }
+}, {
+  offset: '100%' // triggers when 90% of the element is visible
+});
